808e2c5ef1d3d9378d75d7c54de570bb5bd45980
Agony Midnight <agony.midnight.am@gmail.com>
2020-03-10 18:53:55 +0300

    try to make spinner

--- app/src/main/java/com/example/clinic/timetable/TimetableViewModel.kt
+++ app/src/main/java/com/example/clinic/timetable/TimetableViewModel.kt
@@ -1,13 +1,36 @@
 package com.example.clinic.timetable
 
 import android.widget.Spinner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TimetableViewModel : ViewModel() {
-    val spinnerOfTerapist: MutableLiveData<Spinner> = MutableLiveData()
-    val spinnerOfSurgeon: MutableLiveData<Spinner> = MutableLiveData()
-    val spinnerOfNeurologist: MutableLiveData<Spinner> = MutableLiveData()
+    private val _spinnerOfTerapist: List<Doctors> = listOf(
+        Doctors("Петров В.А",1235),
+        Doctors("Тимонина Д.Д", 464),
+        Doctors("Кравец П.п",1111)
 
+    )
+    val _spinnerOfSurgeon: List<Doctors> = listOf(
+        Doctors("Петров В.А",1235),
+        Doctors("Тимонина Д.Д", 464),
+        Doctors("Кравец П.п",1111)
+
+    )
+    var SelDoctors: Doctors? = null
+    private val _spinnerOfNeurologist:List<Doctors> = listOf(
+        Doctors("Петров В.А",1235),
+        Doctors("Тимонина Д.Д", 464),
+        Doctors("Кравец П.п",1111)
+
+
+
+    )
+    val spinnerOfTerapist: LiveData<List<Doctors>> = MutableLiveData(_spinnerOfTerapist)
+    val spinnerOfSurgeon: LiveData<List<Doctors>> = MutableLiveData(_spinnerOfSurgeon)
+    val spinnerOfNeurologist: LiveData<List<Doctors>> = MutableLiveData(_spinnerOfNeurologist)
+
+    class Doctors(val FIO:String, val id:Int)
 }
 
--- app/src/main/java/com/example/clinic/timetable/timetable.kt
+++ app/src/main/java/com/example/clinic/timetable/timetable.kt
@@ -6,19 +6,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.example.clinic.R
 //import com.example.clinic.personal_account.personal_account
+import com.example.clinic.databinding.TimetableFragmentBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.timetable_fragment.*
 
 class timetable : Fragment() {
 
-   // companion object {
-     //   fun newInstance() = timetable()
-   // }
-
-   // private lateinit var viewModel: TimetableViewModel
+    private lateinit var dataBinding:TimetableFragmentBinding
+    private val viewModel: TimetableViewModel by viewModels{
+        object: ViewModelProvider.Factory{
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+                TimetableViewModel() as T
+        }
+    }
 
     override fun onCreateView(
 
@@ -28,16 +38,36 @@ class timetable : Fragment() {
 
     ): View? {
 
-        return inflater.inflate(R.layout.timetable_fragment, container, false)
+        dataBinding = TimetableFragmentBinding.inflate(inflater, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
+        dataBinding.viewModel = viewModel
+        dataBinding.lifecycleOwner = viewLifecycleOwner
 
-        //viewModel = ViewModelProviders.of(this).get(TimetableViewModel::class.java)
-        // TODO: Use the ViewModel
+        dataBinding.spinerofterapist.setOnItemClickListener { _, _, position, _ ->
+            viewModel.
+            spinnerOfTerapist.
+            value?.
+            getOrNull(position)?.
+            let { viewModel.SelDoctors = it }
+        }
+
+        viewModel.spinnerOfTerapist.observe(viewLifecycleOwner, Observer ( ::displayModels ))
     }
+    private fun displayModels(models:List<TimetableViewModel.Doctors>?){
+        spinerofterapist ?: return
+        dataBinding.spinerofterapist.adapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.activity_list_item,
+            spinerofterapist.map{ it.title}
+        )
 
+    }
 }
+
+
